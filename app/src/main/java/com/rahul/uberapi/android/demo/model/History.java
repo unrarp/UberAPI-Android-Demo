4b652ef4b4b5e829f7e40ee9b0da59231f1dc529
Rahul Parsani <rahul.parsani@gmail.com>
2014-09-03 11:28:22 +0800

    Better comments for the user activity endpoint models

--- app/src/main/java/com/rahul/uberapi/android/demo/model/History.java
+++ app/src/main/java/com/rahul/uberapi/android/demo/model/History.java
@@ -7,54 +7,82 @@ package com.rahul.uberapi.android.demo.model;
  */
 public class History extends UberModel {
 
+    /**
+     * Unique user identifier.
+     */
     String uuid;
 
     public String getUUID() {
         return uuid;
     }
 
+    /**
+     * Unix timestamp of trip request time.
+     */
     long request_time;
 
     public long getRequestTime() {
         return request_time;
     }
 
+    /**
+     * Unique identifier representing a specific product for a given latitude & longitude. For
+     * example, uberX in San Francisco will have a different product_id than uberX in Los Angeles.
+     */
     String product_id;
 
     public String getProductId() {
         return product_id;
     }
 
+    /**
+     * Status of the trip. Only returns completed for now.
+     */
     String status;
 
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Length of trip in miles.
+     */
     float distance;
 
     public float getDistance() {
         return distance;
     }
 
+    /**
+     * Unix timestamp of trip start time.
+     */
     long start_time;
 
     public long getStart_time() {
         return start_time;
     }
 
+    /**
+     * Latitude, longitude & address of the start location.
+     */
     Location start_location;
 
     public Location getStartLocation() {
         return start_location;
     }
 
+    /**
+     * Unix timestamp of trip end time.
+     */
     long end_time;
 
     public long getEndTime() {
         return end_time;
     }
 
+    /**
+     * Latitude, longitude & address of the end location.
+     */
     Location end_location;
 
     public Location getEndLocation() {
--- app/src/main/java/com/rahul/uberapi/android/demo/model/Location.java
+++ app/src/main/java/com/rahul/uberapi/android/demo/model/Location.java
@@ -1,19 +1,32 @@
 package com.rahul.uberapi.android.demo.model;
 
+/**
+ * Used by the user activity endpoint and contains information including the latitude, longitude &
+ * address of a location.
+ */
 public class Location extends UberModel {
 
+    /**
+     * Human-readable address.
+     */
     String address;
 
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Latitude component of location.
+     */
     double latitude;
 
     public double getLatitude() {
         return latitude;
     }
 
+    /**
+     * Longitude component of location.
+     */
     double longitude;
 
     public double getLongitude() {
